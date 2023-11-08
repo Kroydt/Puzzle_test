f723deaaa9088bcbb8e563f286f3cb851b08de03
Kroydt <150144698+Kroydt@users.noreply.github.com>
2023-11-08 10:11:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+const pieces = document.querySelectorAll(".piece");
+
+pieces.forEach(piece => {
+    piece.addEventListener("click", () => {
+        // Gérez le déplacement des pièces ici
+    });
+});
